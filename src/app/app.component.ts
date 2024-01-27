3c51c78f7a01af3985fd13bda356050fbcb2a94a
lkuessner <leonkuessner@outlook.com>
2024-01-27 10:35:38 +0100

    feat: default startup screen with counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,43 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>Welcome to {{title}}!</h1>
 
-    <h2>Testing triggers</h2>
+    <h2>Current count is {{ count }}</h2>
+    <div class="btn-container">
+      <button (click)="increment()">Increment</button>
+      <button (click)="decrement()">Decrement</button>
+    </div>
     <router-outlet />
   `,
-  styles: [],
+  styles: [`
+    :host {
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+    
+    .btn-container {
+      width: 500px;
+      height: 50px;
+      display: flex;
+      
+      button {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  `],
 })
 export class AppComponent {
   title = 'gatorux.com';
+  count: number = 0;
+
+  increment(){
+    this.count += 1
+  }
+
+  decrement(){
+    this.count -= 1
+  }
 }
